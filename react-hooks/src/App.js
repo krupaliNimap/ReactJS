ee3c3582dc8ebf4176ec257d35b6f6849bc632cd
krupaliNimap <krupali@nimapinfotech.com>
2023-07-03 19:17:42 +0530

    react hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,32 +1,37 @@
+import React from "react";
 import "./App.css";
-import HooksCounter from "./components/usestate/HooksCounter";
-import HooksCounterTwo from "./components/usestate/HooksCounterTwo";
-import HooksCounterThree from "./components/usestate/HooksCounterThree";
-import HooksCounterFour from "./components/usestate/HooksCounterFour";
-import HookExampleOne from "./components/useeffect/HookExampleOne";
-import HookExampleTwo from "./components/useeffect/HookExampleTwo";
-import HookExampleThree from "./components/useeffect/HookExampleThree";
-import HookExampleFour from "./components/useeffect/HookExampleFour";
-
+import HookExample from "./components/HookExample";
+// import HooksCounter from "./components/usestate/HooksCounter";
+// import HooksCounterTwo from "./components/usestate/HooksCounterTwo";
+// import HooksCounterThree from "./components/usestate/HooksCounterThree";
+// import HooksCounterFour from "./components/usestate/HooksCounterFour";
+// import HookExampleOne from "./components/useeffect/HookExampleOne";
+// import HookExampleTwo from "./components/useeffect/HookExampleTwo";
+// import HookExampleThree from "./components/useeffect/HookExampleThree";
+// import HookExampleFour from "./components/useeffect/HookExampleFour";
 function App() {
   return (
-    <div className="App">
-      <HooksCounter></HooksCounter>
-      <hr></hr>
-      <HooksCounterTwo></HooksCounterTwo>
-      <hr></hr>
-      <HooksCounterThree></HooksCounterThree>
-      <hr></hr>
-      <HooksCounterFour></HooksCounterFour>
-      <hr></hr>
-      <HookExampleOne></HookExampleOne>
-      <hr></hr>
-      <HookExampleTwo></HookExampleTwo>
-      <hr></hr>
-      <HookExampleThree></HookExampleThree>
-      <hr></hr>
-      <HookExampleFour></HookExampleFour>
+    <div>
+      <p>Hello</p>
+      <HookExample />
     </div>
+    // <div className="App">
+    //   <HooksCounter></HooksCounter>
+    //   <hr></hr>
+    //   <HooksCounterTwo></HooksCounterTwo>
+    //   <hr></hr>
+    //   <HooksCounterThree></HooksCounterThree>
+    //   <hr></hr>
+    //   <HooksCounterFour></HooksCounterFour>
+    //   <hr></hr>
+    //   <HookExampleOne></HookExampleOne>
+    //   <hr></hr>
+    //   <HookExampleTwo></HookExampleTwo>
+    //   <hr></hr>
+    //   <HookExampleThree></HookExampleThree>
+    //   <hr></hr>
+    //   <HookExampleFour></HookExampleFour>
+    // </div>
   );
 }
 
